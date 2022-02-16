ced7b9018dfab3257e2aa40e056fbd44a94ed27f
tiberiusss26 <titirigatibi26@gmail.com>
2022-02-16 11:28:38 +0200

    proiect0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+define PI 3.14159
+class forma{
+    public:
+      string form;
+      int f,g;
+};
+class cerc: public forma{
+    int r;
+    int h;
+    cin>>r>>h;
+    g = r*r*PI*h;
+    f =
+    void afisare(){cout<<"frisca:"<<f<<endl<<"gem:"<<g;}
+
+};
+class patrat : public forma{
+
+};
+class dreptunghi:public forma{
+
+};
+class triunghi :public forma{
+
+};
 int main() {
     int a, b;
     cin>>a>>b;
