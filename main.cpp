6388ad317c16eb7ebb5ca02b1eb1d75a19f2a96d
tiberiusss26 <titirigatibi26@gmail.com>
2022-02-23 13:05:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-define PI 3.14159
+define PI 3.14159;
 class forma{
     public:
       string form;
       int f,g;
+      void afisare(){cout<<"frisca:"<<forma.f<<endl<<"gem:"<<forma.g;}
 };
 class cerc: public forma{
     int r;
@@ -13,11 +14,14 @@ class cerc: public forma{
     cin>>r>>h;
     g = r*r*PI*h;
     f =
-    void afisare(){cout<<"frisca:"<<f<<endl<<"gem:"<<g;}
 
 };
 class patrat : public forma{
-
+    int l;
+    int h;
+    cin>>l>>h;
+    g = l*l*h;
+    f = 6*l*l;
 };
 class dreptunghi:public forma{
 
@@ -28,5 +32,4 @@ class triunghi :public forma{
 int main() {
     int a, b;
     cin>>a>>b;
-    cout<<a+b;
 }
